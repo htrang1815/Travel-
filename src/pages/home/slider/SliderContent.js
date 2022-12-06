e7e3b069ba10fd23b6b1cffa316e937862a6056e
htrang1815 <htrang1815@gmail.com>
2022-12-06 23:56:11 +0700

    fe

--- src/pages/home/slider/SliderContent.js
+++ src/pages/home/slider/SliderContent.js
@@ -17,8 +17,7 @@ const SliderContent = ({ className }) => {
           Travel isn’t always pretty. It isn’t always comfortable. Sometimes it
           hurts, it even breaks your heart. But that’s okay. The journey changes
           you; it should change you. It leaves marks on your memory, on your
-          consciousness, on your heart, and on your body. You take something
-          with you. Hopefully, you leave something good behind.
+          consciousness, on your heart, and on your body. Hopefully, you leave something good behind.
         </p>
         <ButtonDetailProjects
           className="mt-3 border-[#111] text-[#111]"
